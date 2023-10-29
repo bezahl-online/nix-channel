ac234ab9a509061a65229f7308f9259dffebc3ca
Ralph Eichelberger <ralph.eichelberger@gmail.com>
2023-10-29 17:08:18 +0100

    .

--- default.nix
+++ default.nix
@@ -1,15 +1,12 @@
 let
+  # Import sources
   sources = import ./nix/sources.nix;
-
-  hello = pkgs.writeShellScriptBin "hello" ''
-    echo "Hello from the Nix channel overlay!"
-  '';
-
-  pkgs = import sources.nixpkgs {
-    overlays = [
-      (self: super: {
-        inherit hello;
-      })
-    ];
-  };
-in pkgs
+  pkgs = import sources.nixpkgs { };
+  
+  # Import your packages
+  ptapi = import ./ptapi { inherit pkgs; };
+  RFID = import ./RFID { inherit pkgs; };
+# And return that specific nixpkgs
+in  {
+  inherit ptapi RFID;
+}
